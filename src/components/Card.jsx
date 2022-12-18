7cad0e20f9c9fe8429c501d91849aca81ae5a406
veyseltosun <postalarim1923@gmail.com>
2022-12-18 13:53:18 +0300

    initial commit.

---
+++ src/components/Card.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import Email from "./Email"
+import Phone from "./Phone";
+import Location from "./Location";
+import axios from 'axios';
+import {useState, useEffect} from "react";
+import "./Card.css"
+
+const API_URL = "https://randomuser.me/api/"
+ 
+
+const Card = () => {
+
+    const [user, setUser] = useState("");
+
+
+    const getRandomUser =  () =>{
+
+        axios.get(API_URL).then((response) => {
+            console.log(response.data.results[0]);
+            const userData = response.data.results[0]
+            setUser(userData)
+        })
+
+    };
+
+    useEffect(() => {
+        getRandomUser()
+
+    },[])
+    
+
+
+
+
+
+
+  return (
+    <div className='container'>
+        <div className='card'>
+            <div className='image-title-box'>
+                <img className='img' src={user.picture?.large} alt="user picture"></img>
+                <p className='info'>{user.name?.title} {user.name?.first} {user.name?.last}</p>
+                
+            </div>
+            <div className='user-email'>
+                <Email/>
+                <p className='info'>{user.email}</p>
+
+            </div>
+            <div className='user-phone'>
+                <Phone/>
+                <p className='info'>{user.phone}</p>
+
+            </div>
+            <div className='user-location'>
+                <Location/>
+                <p className='info'>{user.location?.city} / {user.location?.country}</p>
+
+            </div>
+            <div className='user-register'>
+                <p className='info'>Age: {user.dob?.age}</p>
+                <p className='info'>Register Date:{user.registered?.date.slice(0,10)} </p>
+
+            </div>
+
+        </div>
+        <div>
+            <button className='button' onClick={getRandomUser}>Random User</button>
+        </div>
+    </div>
+  )
+}
+
+export default Card
